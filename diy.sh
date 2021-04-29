e9fa1c9e228335ebab7f9f081716c6cdd4d56351
ynxuning <83376449+ynxuning@users.noreply.github.com>
2021-04-29 20:23:53 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,5 +7,4 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed  -i  's / 192.168.1.1 / 10.10.10.1 / g'包/基本文件/文件/ bin / config_generate
-
+SED -i ' S / 192.168.1.1 / 192.168.123.99 /克'包/基层-文件/文件/ bin中/ config_generate
